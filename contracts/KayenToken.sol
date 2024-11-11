46cc5257dda6a9bef12625a5e9958cca7c09dae3
0xDanielH <holyhansss@gmail.com>
2024-11-11 17:48:19 +0900

    fix: update MAX_EMISSION_RATE

--- contracts/KayenToken.sol
+++ contracts/KayenToken.sol
@@ -16,7 +16,7 @@ import "./interfaces/IKayenToken.sol";
 contract KayenToken is Ownable, ERC20("Kayen token", "KAYEN"), IKayenToken {
   using SafeMath for uint256;
 
-  uint256 public constant MAX_EMISSION_RATE = 0.01 ether;
+  uint256 public constant MAX_EMISSION_RATE = 5 ether;
   uint256 public constant MAX_SUPPLY_LIMIT = 88000000 ether;
   uint256 public elasticMaxSupply; // Once deployed, controlled through governance only
   uint256 public emissionRate; // Token emission per second
